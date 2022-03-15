16dd46e44dd2ea795c631aecd8f56c879aff1915
Marko Petrlić <petrlic.marko@gmail.com>
2022-03-15 10:00:50 +0100

    Features/372 implement mandate (#9)
    * Added mandate pallet
    
    * Removed Readme

---
+++ mandate/src/lib.rs
@@ -0,0 +1,67 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+
+pub use pallet::*;
+
+use frame_support::{
+	dispatch::{DispatchResultWithPostInfo, GetDispatchInfo, UnfilteredDispatchable},
+	pallet_prelude::*,
+	traits::StorageVersion,
+};
+use frame_system::pallet_prelude::*;
+
+const STORAGE_VERSION: StorageVersion = StorageVersion::new(1);
+
+#[frame_support::pallet]
+pub mod pallet {
+	use super::*;
+
+	/// Configure the pallet by specifying the parameters and types on which it depends.
+	#[pallet::config]
+	pub trait Config: frame_system::Config {
+		/// Because this pallet emits events, it depends on the runtime's definition of an event.
+		type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
+
+		/// A sudo-able call.
+		type Call: Parameter + UnfilteredDispatchable<Origin = Self::Origin> + GetDispatchInfo;
+
+		// Someone who can call the mandate extrinsic.
+		type ExternalOrigin: EnsureOrigin<Self::Origin>;
+	}
+
+	#[pallet::pallet]
+	#[pallet::generate_store(pub(super) trait Store)]
+	#[pallet::storage_version(STORAGE_VERSION)]
+	#[pallet::without_storage_info]
+	pub struct Pallet<T>(_);
+
+	#[pallet::hooks]
+	impl<T: Config> Hooks<BlockNumberFor<T>> for Pallet<T> {}
+
+	#[pallet::call]
+	impl<T: Config> Pallet<T> {
+		#[pallet::weight({
+			let dispatch_info = call.get_dispatch_info();
+			(dispatch_info.weight.saturating_add(10_000), dispatch_info.class)
+		})]
+		pub fn mandate(
+			origin: OriginFor<T>,
+			call: Box<<T as Config>::Call>,
+		) -> DispatchResultWithPostInfo {
+			T::ExternalOrigin::ensure_origin(origin)?;
+
+			let res = call.dispatch_bypass_filter(frame_system::RawOrigin::Root.into());
+
+			let result = res.map(|_| ()).map_err(|e| e.error);
+			Self::deposit_event(Event::RootOp { result });
+
+			// Free action :)
+			Ok(Pays::No.into())
+		}
+	}
+	#[pallet::event]
+	#[pallet::generate_deposit(pub(super) fn deposit_event)]
+	pub enum Event<T: Config> {
+		/// A root operation was executed, show result
+		RootOp { result: DispatchResult },
+	}
+}
