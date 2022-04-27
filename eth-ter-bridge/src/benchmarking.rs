c809a9c000c33c691990db3adfb9c698af66cfdf
Marko Petrlic <petrlic.marko@gmail.com>
2022-04-27 02:28:43 +0200

    Adjusted benchmarks

--- eth-ter-bridge/src/benchmarking.rs
+++ eth-ter-bridge/src/benchmarking.rs
@@ -14,8 +14,6 @@
 // You should have received a copy of the GNU General Public License
 // along with Ternoa.  If not, see <http://www.gnu.org/licenses/>.
 
-#![cfg(feature = "runtime-benchmarks")]
-
 use super::*;
 use frame_benchmarking::{account as benchmark_account, benchmarks, impl_benchmark_test_suite};
 use frame_support::{assert_ok, bounded_vec, traits::Currency};
@@ -23,7 +21,9 @@ use frame_system::RawOrigin;
 use sp_runtime::traits::Bounded;
 use sp_std::prelude::*;
 
-use crate::Pallet as Bridge;
+use crate::{ChainId, Pallet as Bridge};
+
+const CHAIN_ID: ChainId = 15;
 
 pub fn get_account<T: Config>(name: &'static str) -> T::AccountId {
 	let account: T::AccountId = benchmark_account(name, 0, 0);
@@ -38,115 +38,92 @@ pub fn prepare_benchmarks<T: Config>() {
 	T::Currency::make_free_balance_be(&relayer_a, BalanceOf::<T>::max_value() / 5u32.into());
 	T::Currency::make_free_balance_be(&relayer_b, BalanceOf::<T>::max_value() / 5u32.into());
 	T::Currency::make_free_balance_be(&relayer_c, BalanceOf::<T>::max_value() / 5u32.into());
+
+	assert_ok!(Bridge::<T>::add_chain(RawOrigin::Root.into(), CHAIN_ID));
 }
 
 benchmarks! {
 	set_threshold {
 		prepare_benchmarks::<T>();
-
-		let root = RawOrigin::Root;
 		let threshold = 3;
 
-	}: _(root, threshold)
+	}: _(RawOrigin::Root, threshold)
 	verify {
 		assert_eq!(RelayerVoteThreshold::<T>::get(), threshold);
 	}
 
 	add_chain {
 		prepare_benchmarks::<T>();
-
-		let root = RawOrigin::Root;
 		let chain_id = 0;
 
-	}: _(root, chain_id)
+	}: _(RawOrigin::Root, chain_id)
 	verify {
 		assert!(ChainNonces::<T>::get(chain_id).is_some());
 	}
 
 	set_relayers {
 		prepare_benchmarks::<T>();
-
-		let root = RawOrigin::Root;
 		let relayer_a = get_account::<T>("RELAYER_A");
 		let relayer_b = get_account::<T>("RELAYER_B");
 		let relayer_c = get_account::<T>("RELAYER_C");
 		let relayers: BoundedVec<T::AccountId, T::RelayerCountLimit> = bounded_vec![relayer_a, relayer_b, relayer_c];
 
-	}: _(root, relayers.clone())
+	}: _(RawOrigin::Root, relayers.clone())
 	verify {
 		assert_eq!(Relayers::<T>::get(), relayers);
 	}
 
 	vote_for_proposal {
 		prepare_benchmarks::<T>();
-
-		let root = RawOrigin::Root;
 		let relayer_a: T::AccountId = get_account::<T>("RELAYER_A");
-		assert_ok!(Bridge::<T>::set_relayers(root.clone().into(), bounded_vec![relayer_a.clone()]));
+		assert_ok!(Bridge::<T>::set_relayers(RawOrigin::Root.into(), bounded_vec![relayer_a.clone()]));
 
 		let recipient: <T::Lookup as StaticLookup>::Source = T::Lookup::unlookup(get_account::<T>("RELAYER_C"));
-		let chain_id = 0;
-
-		assert_ok!(Bridge::<T>::add_chain(root.clone().into(), chain_id));
-
-		let amount = 100u32;
-		let deposit_nonce = ChainNonces::<T>::get(chain_id).unwrap();
-		// let proposal = Bridge::get_votes(chain_id, (deposit_nonce, recipient, amount));
-
-	}: _(RawOrigin::Signed(relayer_a), chain_id, deposit_nonce, recipient, amount.into(), true)
+		let amount: BalanceOf<T> = 100u32.into();
+		let deposit_nonce = ChainNonces::<T>::get(CHAIN_ID).unwrap();
+		let relayer_c: T::AccountId = get_account::<T>("RELAYER_C");
+	}: _(RawOrigin::Signed(relayer_a.clone()), CHAIN_ID, deposit_nonce, recipient.clone(), amount.clone())
 	verify {
-		// let proposal = Bridge::get_votes(chain_id, (deposit_nonce, recipient, amount));
-		// assert!(proposal.is_some());
-		// let count = proposal.unwrap().votes.iter().filter(|x| x.1 == true).count() as u32;
-		// assert_eq!(count, 1);
+		let proposal = Bridge::<T>::get_votes(CHAIN_ID, (deposit_nonce, relayer_c, amount)).unwrap();
+		assert_eq!(proposal.votes.len(), 1);
 	}
 
 	deposit {
 		prepare_benchmarks::<T>();
-
-		let root = RawOrigin::Root;
 		let relayer_a: T::AccountId = get_account::<T>("RELAYER_A");
 		let collector: T::AccountId = get_account::<T>("COLLECTOR");
+		let relayer_a_old_balance = T::Currency::free_balance(&relayer_a);
+
 		let amount: BalanceOf<T> = 10u32.into();
-		let chain_id = 1;
 		let bridge_fee: BalanceOf<T> = 100u32.into();
 		let recipient = vec![0];
-		let deposit_nonce = Bridge::<T>::chain_nonces(chain_id);
-		// let total_issuance: BalanceOf<T> = BalanceOf::total_issuance();
-		// let collector_before = Balances::free_balance(&collector);
-		// let relayer_a_balance_before: BalanceOf<T> = BalanceOf::<T>::free_balance(&relayer_a);
+		let deposit_nonce = Bridge::<T>::chain_nonces(CHAIN_ID);
+		assert_ok!(Bridge::<T>::set_bridge_fee(RawOrigin::Root.into(), bridge_fee));
 
-		assert_ok!(Bridge::<T>::add_chain(root.clone().into(), chain_id));
-		assert_ok!(Bridge::<T>::set_bridge_fee(root.into(), bridge_fee));
-
-	}: _(RawOrigin::Signed(relayer_a), amount.clone().into(), recipient, chain_id)
+	}: _(RawOrigin::Signed(relayer_a.clone()), amount.clone().into(), recipient, CHAIN_ID)
 	verify {
-		// assert_eq!(BalanceOf::total_issuance(), total_issuance.into() - amount.into());
-		// assert_eq!(BalanceOf::free_balance(&relayer_a), relayer_a_balance_before - amount - bridge_fee);
-		// assert_eq!(BalanceOf::free_balance(&collector), collector_before + bridge_fee.into());
-		// assert_eq!(Bridge::chain_nonces(chain_id).unwrap(), deposit_nonce.unwrap() + 1);
+		assert_eq!(T::Currency::free_balance(&relayer_a), relayer_a_old_balance - amount - bridge_fee);
+		assert_ne!(T::Currency::free_balance(&relayer_a), relayer_a_old_balance);
+		assert_ne!(amount, 0u32.into());
+		assert_ne!(bridge_fee, 0u32.into());
 	}
 
 	set_bridge_fee {
 		prepare_benchmarks::<T>();
-
-		let root = RawOrigin::Root;
 		let bridge_fee = 100u32;
 
-	}: _(root, bridge_fee.clone().into())
+	}: _(RawOrigin::Root, bridge_fee.clone().into())
 	verify {
 		assert_eq!(BridgeFee::<T>::get(), bridge_fee.into());
 	}
 
 	set_deposit_nonce {
 		prepare_benchmarks::<T>();
+		let nonce = 1;
 
-		let root = RawOrigin::Root;
-		let bridge_fee = 100u32;
-
-	}: _(root, bridge_fee.clone().into())
+	}: _(RawOrigin::Root, CHAIN_ID, nonce)
 	verify {
-		assert_eq!(BridgeFee::<T>::get(), bridge_fee.into());
+		assert_eq!(ChainNonces::<T>::get(CHAIN_ID), Some(nonce));
 	}
 }
 
