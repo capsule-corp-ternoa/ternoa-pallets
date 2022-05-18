a9c993f18db7714754c76eae3dfb191b64e37035
sgalasso <simon.galasso@hotmail.fr>
2022-05-18 09:29:50 +0200

    capsule happy split + clean

--- capsule/src/tests/extrinsics.rs
+++ capsule/src/tests/extrinsics.rs
@@ -36,20 +36,19 @@ pub mod create {
 	#[test]
 	fn create() {
 		ExtBuilder::new_build(vec![(ALICE, 10000)]).execute_with(|| {
-			let nft_id = 0;
+			let alice: mock::Origin = origin(ALICE);
+			let alice_balance_before = Balances::free_balance(ALICE);
 			let capsule_mint_fee = Capsule::capsule_mint_fee();
 			let nft_fee = NFT::nft_mint_fee();
-			let alice_balance_before = Balances::free_balance(ALICE);
+			let nft_id = 0;
+			let ipfs_reference: CapsuleIPFSReference<Test> = bounded_vec![60];
 
-			let ok = Capsule::create(origin(ALICE), bounded_vec![50], bounded_vec![60], None);
+			let ok = Capsule::create(alice, bounded_vec![50], ipfs_reference.clone(), None);
 			assert_ok!(ok);
 
 			// Storage
-			assert_eq!(Capsule::capsules(&nft_id), Some(CapsuleData::new(ALICE, bounded_vec![60])));
-			assert_eq!(
-				Capsule::ledgers(&ALICE),
-				Some(bounded_vec![(nft_id, Capsule::capsule_mint_fee())])
-			);
+			assert_eq!(Capsule::capsules(&nft_id), Some(CapsuleData::new(ALICE, ipfs_reference)));
+			assert_eq!(Capsule::ledgers(&ALICE), Some(bounded_vec![(nft_id, capsule_mint_fee)]));
 			assert_eq!(
 				Balances::free_balance(ALICE),
 				alice_balance_before - capsule_mint_fee - nft_fee
@@ -68,32 +67,6 @@ pub mod create {
 	}
 
 	#[test]
-	fn create_transactional() {
-		ExtBuilder::new_build(vec![(ALICE, 10002), (BOB, 10002)]).execute_with(|| {
-			let alice: mock::Origin = origin(ALICE);
-			let balance = Balances::free_balance(ALICE);
-			let capsule_fee = Capsule::capsule_mint_fee();
-			let nft_fee = NFT::nft_mint_fee();
-			let pallet_id = Capsule::account_id();
-
-			// Lets make sure that Alice has enough funds
-			assert!(balance > (capsule_fee + nft_fee));
-
-			let series_id = Some("AAA".into());
-			let ok = NFT::create_nft(BOB, bounded_vec![], series_id.clone());
-			assert_ok!(ok);
-
-			// Trigger an error
-			let ok = Capsule::create(alice.clone(), bounded_vec![], bounded_vec![], series_id);
-			assert_noop!(ok, ternoa_nft::Error::<Test>::NotTheSeriesOwner);
-
-			// She should not have lost any caps
-			assert_eq!(Balances::free_balance(ALICE), balance);
-			assert_eq!(Balances::free_balance(pallet_id), 0);
-		})
-	}
-
-	#[test]
 	fn insufficient_balance() {
 		ExtBuilder::new_build(vec![(BOB, 100)]).execute_with(|| {
 			let ok = Capsule::create(origin(BOB), bounded_vec![], bounded_vec![1], None);
@@ -108,40 +81,34 @@ pub mod create_from_nft {
 	#[test]
 	fn create_from_nft() {
 		ExtBuilder::new_build(vec![(ALICE, 10000)]).execute_with(|| {
-			// Initial state
 			let alice: mock::Origin = origin(ALICE);
+			let alice_balance_before = Balances::free_balance(ALICE);
+			let capsule_mint_fee = Capsule::capsule_mint_fee();
+			let nft_fee = NFT::nft_mint_fee();
 			let nft_id = help::create_nft_fast(alice.clone());
-			let ipfs_reference: BoundedVec<u8, IPFSLengthLimit> = bounded_vec![60];
-			assert_eq!(Capsule::capsules(&nft_id), None);
-			assert_eq!(Capsule::ledgers(&ALICE), None);
-
-			// Happy path
-			let data = CapsuleData::new(ALICE, ipfs_reference.clone());
-			let ledger = bounded_vec![(nft_id, Capsule::capsule_mint_fee())];
-
-			let ok = Capsule::create_from_nft(alice.clone(), nft_id, ipfs_reference);
-			assert_ok!(ok);
-			assert_eq!(Capsule::capsules(&nft_id), Some(data));
-			assert_eq!(Capsule::ledgers(&ALICE), Some(ledger));
-		})
-	}
-
-	#[test]
-	fn create_from_nft_caps_transfer() {
-		ExtBuilder::new_build(vec![(ALICE, 10001)]).execute_with(|| {
-			let alice: mock::Origin = origin(ALICE);
-			let capsule_fee = Capsule::capsule_mint_fee();
 			let pallet_id = Capsule::account_id();
-			assert_ne!(capsule_fee, 0);
-			assert_eq!(Balances::free_balance(pallet_id), 0);
+			let ipfs_reference: BoundedVec<u8, IPFSLengthLimit> = bounded_vec![60];
 
-			// Funds are transferred
-			let nft_id = help::create_nft_fast(alice.clone());
-			let balance = Balances::free_balance(ALICE);
-			let ok = Capsule::create_from_nft(alice.clone(), nft_id, bounded_vec![50]);
+			let ok = Capsule::create_from_nft(alice, nft_id, ipfs_reference.clone());
 			assert_ok!(ok);
-			assert_eq!(Balances::free_balance(ALICE), balance - capsule_fee);
-			assert_eq!(Balances::free_balance(pallet_id), capsule_fee);
+
+			// Storage
+			assert_eq!(Capsule::capsules(&nft_id), Some(CapsuleData::new(ALICE, ipfs_reference)));
+			assert_eq!(Capsule::ledgers(&ALICE), Some(bounded_vec![(nft_id, capsule_mint_fee)]));
+			assert_eq!(
+				Balances::free_balance(ALICE),
+				alice_balance_before - capsule_mint_fee - nft_fee
+			);
+			assert_eq!(Balances::free_balance(pallet_id), capsule_mint_fee);
+
+			// Event
+			let event = CapsuleEvent::CapsuleCreated {
+				owner: ALICE,
+				nft_id,
+				frozen_balance: capsule_mint_fee,
+			};
+			let event = Event::Capsule(event);
+			assert_eq!(System::events().last().unwrap().event, event);
 		})
 	}
 
@@ -159,7 +126,7 @@ pub mod create_from_nft {
 	fn cannot_create_capsules_from_nft_listed_for_sale() {
 		ExtBuilder::new_build(vec![(ALICE, 10000)]).execute_with(|| {
 			let alice: mock::Origin = origin(ALICE);
-			let nft_id = help::create_nft_fast(alice.clone());
+			let nft_id = help::create_nft_fast(origin(ALICE));
 			let ok = <NFT as NFTExt>::set_listed_for_sale(nft_id, true);
 			assert_ok!(ok);
 
@@ -212,43 +179,26 @@ pub mod remove {
 	#[test]
 	fn remove() {
 		ExtBuilder::new_build(vec![(ALICE, 10000)]).execute_with(|| {
-			// Initial state
 			let alice: mock::Origin = origin(ALICE);
-			let nft_id_1 = help::create_capsule_fast(alice.clone());
-			let nft_id_2 = help::create_capsule_fast(alice.clone());
-			let ledger = bounded_vec![(nft_id_2, Capsule::capsule_mint_fee())];
-
-			// Happy path delete one nft id associated with that owner
-			let ok = Capsule::remove(alice.clone(), nft_id_1);
-			assert_ok!(ok);
-			assert_eq!(Capsule::capsules(&nft_id_1), None);
-			assert_eq!(Capsule::ledgers(&ALICE), Some(ledger));
-
-			// Happy path delete last nft id associated with that owner
-			let ok = Capsule::remove(alice.clone(), nft_id_2);
-			assert_ok!(ok);
-			assert_eq!(Capsule::capsules(&nft_id_2), None);
-			assert_eq!(Capsule::ledgers(&ALICE), None);
-		})
-	}
-
-	#[test]
-	fn remove_caps_transfer() {
-		ExtBuilder::new_build(vec![(ALICE, 10000)]).execute_with(|| {
-			let alice: mock::Origin = origin(ALICE);
-
 			let nft_id = help::create_capsule_fast(alice.clone());
-			let fee = Capsule::ledgers(ALICE).unwrap()[0].1;
+			let capsule_mint_fee = Capsule::capsule_mint_fee();
 			let pallet_id = Capsule::account_id();
+			let pallet_balance_before = Balances::free_balance(pallet_id);
+			let alice_balance_before = Balances::free_balance(ALICE);
 
-			let pallet_balance = Balances::free_balance(pallet_id);
-			let alice_balance = Balances::free_balance(ALICE);
-
-			// Funds are transferred
-			let ok = Capsule::remove(alice.clone(), nft_id);
+			let ok = Capsule::remove(alice, nft_id);
 			assert_ok!(ok);
-			assert_eq!(Balances::free_balance(ALICE), alice_balance + fee);
-			assert_eq!(Balances::free_balance(pallet_id), pallet_balance - fee);
+
+			// Storage
+			assert_eq!(Capsule::capsules(&nft_id), None);
+			assert_eq!(Capsule::ledgers(&ALICE), None);
+			assert_eq!(Balances::free_balance(ALICE), alice_balance_before + capsule_mint_fee);
+			assert_eq!(Balances::free_balance(pallet_id), pallet_balance_before - capsule_mint_fee);
+
+			// Event
+			let event = CapsuleEvent::CapsuleRemoved { nft_id, unfrozen_balance: capsule_mint_fee };
+			let event = Event::Capsule(event);
+			assert_eq!(System::events().last().unwrap().event, event);
 		})
 	}
 
@@ -283,39 +233,29 @@ pub mod add_funds {
 	#[test]
 	fn add_funds() {
 		ExtBuilder::new_build(vec![(ALICE, 10000)]).execute_with(|| {
-			// Initial state
 			let alice: mock::Origin = origin(ALICE);
 			let nft_id = help::create_capsule_fast(alice.clone());
-			let fee = Capsule::capsule_mint_fee();
-			let ledger = bounded_vec![(nft_id, fee)];
-			assert_eq!(Capsule::ledgers(&ALICE), Some(ledger));
-
-			// Happy path
+			let capsule_mint_fee = Capsule::capsule_mint_fee();
 			let add = 55;
-			let ledger = bounded_vec![(nft_id, fee + add)];
-			let ok = Capsule::add_funds(alice.clone(), nft_id, add);
-			assert_ok!(ok);
-			assert_eq!(Capsule::ledgers(&ALICE), Some(ledger));
-		})
-	}
-
-	#[test]
-	fn add_funds_caps_transfer() {
-		ExtBuilder::new_build(vec![(ALICE, 10001)]).execute_with(|| {
-			let alice: mock::Origin = origin(ALICE);
-
-			let nft_id = help::create_capsule_fast(alice.clone());
 			let pallet_id = Capsule::account_id();
+			let alice_balance_before = Balances::free_balance(ALICE);
+			let pallet_balance_before = Balances::free_balance(pallet_id);
 
-			let alice_balance = Balances::free_balance(ALICE);
-			let pallet_balance = Balances::free_balance(pallet_id);
-
-			// Funds are transferred
-			let add = 1010;
 			let ok = Capsule::add_funds(alice.clone(), nft_id, add);
 			assert_ok!(ok);
-			assert_eq!(Balances::free_balance(ALICE), alice_balance - add);
-			assert_eq!(Balances::free_balance(pallet_id), pallet_balance + add);
+
+			// Storage
+			assert_eq!(
+				Capsule::ledgers(&ALICE),
+				Some(bounded_vec![(nft_id, capsule_mint_fee + add)])
+			);
+			assert_eq!(Balances::free_balance(ALICE), alice_balance_before - add);
+			assert_eq!(Balances::free_balance(pallet_id), pallet_balance_before + add);
+
+			// Event
+			let event = CapsuleEvent::CapsuleFundsAdded { nft_id, balance: add };
+			let event = Event::Capsule(event);
+			assert_eq!(System::events().last().unwrap().event, event);
 		})
 	}
 
@@ -347,7 +287,6 @@ pub mod set_ipfs_reference {
 	#[test]
 	fn set_ipfs_reference() {
 		ExtBuilder::new_build(vec![(ALICE, 10000)]).execute_with(|| {
-			// Initial state
 			let alice: mock::Origin = origin(ALICE);
 			let nft_id = help::create_capsule_fast(alice.clone());
 			let data = Capsule::capsules(nft_id).unwrap();
@@ -355,10 +294,17 @@ pub mod set_ipfs_reference {
 			let new_reference: CapsuleIPFSReference<Test> = bounded_vec![67];
 			assert_ne!(old_reference, new_reference);
 
-			// Happy path
-			let ok = Capsule::set_ipfs_reference(alice.clone(), nft_id, new_reference.clone());
+			let ok = Capsule::set_ipfs_reference(alice, nft_id, new_reference.clone());
 			assert_ok!(ok);
+
+			// Storage
 			assert_eq!(Capsule::capsules(nft_id).unwrap().ipfs_reference, new_reference);
+
+			// Event
+			let event =
+				CapsuleEvent::CapsuleIpfsReferenceUpdated { nft_id, ipfs_reference: new_reference };
+			let event = Event::Capsule(event);
+			assert_eq!(System::events().last().unwrap().event, event);
 		})
 	}
 
@@ -379,14 +325,19 @@ pub mod set_capsule_mint_fee {
 	#[test]
 	fn set_capsule_mint_fee() {
 		ExtBuilder::new_build(vec![]).execute_with(|| {
-			// Happy path
-			let old_mint_fee = Capsule::capsule_mint_fee();
 			let new_mint_fee = 654u128;
-			assert_eq!(Capsule::capsule_mint_fee(), old_mint_fee);
+			assert_ne!(Capsule::capsule_mint_fee(), new_mint_fee);
 
 			let ok = Capsule::set_capsule_mint_fee(root(), new_mint_fee);
 			assert_ok!(ok);
+
+			// Storage
 			assert_eq!(Capsule::capsule_mint_fee(), new_mint_fee);
+
+			// Event
+			let event = CapsuleEvent::CapsuleMintFeeUpdated { fee: new_mint_fee };
+			let event = Event::Capsule(event);
+			assert_eq!(System::events().last().unwrap().event, event);
 		})
 	}
 
