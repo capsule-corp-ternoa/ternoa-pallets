58eab29419414786a6ef40c66e5883c8daaf79c1
ghalielouarzazi@hotmail.com <ghalielouarzazi@hotmail.com>
2022-05-24 16:35:09 +0200

    improve benchmarks

--- nft/src/benchmarking.rs
+++ nft/src/benchmarking.rs
@@ -164,10 +164,11 @@ benchmarks! {
 	add_nft_to_collection {
 		prepare_benchmarks::<T>();
 		let alice: T::AccountId = get_account::<T>("ALICE");
+		let alice_origin = origin::<T>("ALICE");
 		let limit = T::CollectionSizeLimit::get() - 1;
 		for _i in 0..limit {
 			NFT::<T>::create_nft(
-				RawOrigin::Signed(alice.clone()),
+				alice_origin,
 				BoundedVec::default(),
 				Permill::from_parts(0),
 				Some(COLLECTION_ID),
